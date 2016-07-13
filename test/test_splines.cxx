118312d7ea20f497ebadf0de819c8448c44831e7
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2016-07-13 17:44:29 +0200

    activated fixed-point tests in test_splines.cxx

--- test/test_splines.cxx
+++ test/test_splines.cxx
@@ -38,6 +38,7 @@
 #include <string>
 #include <vigra2/unittest.hxx>
 #include <vigra2/math/splines.hxx>
+#include <vigra2/math/fixedpoint.hxx>
 
 using namespace vigra;
 
@@ -61,20 +62,20 @@ struct SplineTest
         }
     }
 
-    // void testFixedPointValues()
-    // {
-        // double r = spline.radius();
-        // shouldEqual(r, splineBase.radius());
+     void testFixedPointValues()
+     {
+         double r = spline.radius();
+         shouldEqual(r, splineBase.radius());
 
-        // for(double x = -r-0.5; x <= r+0.5; x += 0.5)
-        // {
-            // FixedPoint<11,20> fpx20(x);
-            // FixedPoint<11,15> fpx15(x);
-            // should(abs(fixed_point_cast<double>(spline(fpx20)) - spline(x)) < 1e-6);
-            // should(abs(fixed_point_cast<double>(spline(fpx15)) - spline(x)) < 4e-5);
+         for(double x = -r-0.5; x <= r+0.5; x += 0.5)
+         {
+             FixedPoint<11,20> fpx20(x);
+             FixedPoint<11,15> fpx15(x);
+             should(abs(fixed_point_cast<double>(spline(fpx20)) - spline(x)) < 1e-6);
+             should(abs(fixed_point_cast<double>(spline(fpx15)) - spline(x)) < 4e-5);
 
-        // }
-    // }
+         }
+     }
 
     void testPrefilterCoefficients()
     {
@@ -122,11 +123,11 @@ struct SplineTestSuite
         add( testCase(&SplineTest<3>::testValues));
         add( testCase(&SplineTest<4>::testValues));
         add( testCase(&SplineTest<5>::testValues));
-        // add( testCase(&SplineTest<0>::testFixedPointValues));
-        // add( testCase(&SplineTest<1>::testFixedPointValues));
-        // add( testCase(&SplineTest<2>::testFixedPointValues));
-        // add( testCase(&SplineTest<3>::testFixedPointValues));
-        // add( testCase(&SplineTest<0>::testPrefilterCoefficients));
+        add( testCase(&SplineTest<0>::testFixedPointValues));
+        add( testCase(&SplineTest<1>::testFixedPointValues));
+        add( testCase(&SplineTest<2>::testFixedPointValues));
+        add( testCase(&SplineTest<3>::testFixedPointValues));
+        add( testCase(&SplineTest<0>::testPrefilterCoefficients));
         add( testCase(&SplineTest<1>::testPrefilterCoefficients));
         add( testCase(&SplineTest<2>::testPrefilterCoefficients));
         add( testCase(&SplineTest<3>::testPrefilterCoefficients));
